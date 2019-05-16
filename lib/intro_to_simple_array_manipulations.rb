ef1a6477f5f1fdfc771120c2c2940f91ff0fb0d5
Max Sun <max.sun@yale.edu>
2019-05-16 02:21:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,3 +37,11 @@ end
 def using_flatten(array)
   array.flatten
 end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, number)
+  array.delete_at(number)
+end
